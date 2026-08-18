03a588f854bf804ce1c67fec590241be7be30306
wanderlycan <101800125+wanderlycan@users.noreply.github.com>
2026-08-17 21:52:09 -0300

    Delete MyApplication.kt

--- MyApplication.kt
+++
@@ -1,9 +0,0 @@
-import android.app.Application
-
-class MyApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        // Ativa o monitor de falhas global ultraleve
-        AppCrashHandler.init(this)
-    }
-}
